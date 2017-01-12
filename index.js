de56290dc7c334253c94c6240f97aef6676250a3
inxi <276692580@qq.com>
2017-01-12 19:01:42 +0800

    Init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+const exec = require('child_process').execSync;
+const execFile = require('child_process').execFileSync;
+
+var PATH_TO_MYBLOG = '/usr/share/nginx/my-blog/'
+var PATH_TO_MYBLOG_ADMIN = '/usr/share/nginx/my-blog-admin/'
+
+app.post('/my-blog-deploy', function (req, res) {
+    console.log('Begin myblog deploy');
+    console.log(req);
+
+    exec('git pull origin master', {
+        cwd: PATH_TO_MYBLOG
+    }, function (err, stdout, stderr) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+
+    execFile(PATH_TO_MYBLOG + 'build.sh', ['-env production', '-api_region remote'], {
+        cwd: PATH_TO_MYBLOG
+    }, function (err, stdout, stderr) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    })
+});
+
+app.listen('8082', function () {
+    console.log('Deploy server started');
+})
